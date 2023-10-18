a80902ec86851a3a2c1ec065f815b2925e63c609
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-10-18 12:26:30 +0300

    feat: colorize map

--- main.js
+++ main.js
@@ -8,12 +8,54 @@ import View from 'ol/View';
 import Modify from 'ol/interaction/Modify';
 import Draw from 'ol/interaction/Draw';
 import Snap from 'ol/interaction/Snap';
+import colormap from 'colormap';
+import { getArea } from 'ol/sphere';
+import { Style, Fill, Stroke } from 'ol/style';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 
 const osmSource = new OSM();
 const osmLayer = new TileLayer({
     source: osmSource,
+});
+
+const min = 1e8; // the smallest area
+const max = 2e13; // the biggest area
+const steps = 50;
+const ramp = colormap({
+    colormap: 'blackbody',
+    nshades: steps,
+});
+
+function clamp(value, low, high) {
+    return Math.max(low, Math.min(value, high));
+}
+
+function getColor(feature) {
+    const area = getArea(feature.getGeometry());
+    const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
+    const index = Math.round(f * (steps - 1));
+    const opacity = '99';
+    return `${ramp[index]}${opacity}`;
+}
+
+function colorfulAreasStyle(feature) {
+    return new Style({
+        fill: new Fill({
+            color: getColor(feature),
+        }),
+        stroke: new Stroke({
+            color: 'rgba(255,255,255,0.8)',
+        }),
+    });
+}
+const onlyBoundsStyle = new Style({
+    fill: new Fill({
+        color: 'rgba(255,255,255,0.2)',
+    }),
+    stroke: new Stroke({
+        color: 'blue',
+    }),
 });
 
 const customSource = new VectorSource({
@@ -23,6 +65,7 @@ const customSource = new VectorSource({
 
 const customLayer = new VectorLayer({
     source: customSource,
+    style: onlyBoundsStyle,
 });
 
 const map = new Map({
@@ -38,6 +81,12 @@ const showOsmLayerToggle = document.getElementById('osmToggle');
 showOsmLayerToggle.addEventListener('change', function (e) {
     const showOsm = e.target.checked;
     osmLayer.setVisible(showOsm);
+});
+
+const colorizeToggle = document.getElementById('colorizeToggle');
+colorizeToggle.addEventListener('change', function (e) {
+    const needColors = e.target.checked;
+    customLayer.setStyle(needColors ? colorfulAreasStyle : onlyBoundsStyle);
 });
 
 const format = new GeoJSON({ featureProjection: 'EPSG:3857' });
