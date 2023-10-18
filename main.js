c7123b1132325f28ee5c679aabc40fdb5921cad4
Voronin Andrey <Andrey.Voronin@infotecs.ru>
2023-10-18 12:21:29 +0300

    feat: export map data

--- main.js
+++ main.js
@@ -27,6 +27,20 @@ const map = new Map({
     }),
 });
 
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+    source.clear();
+});
+
+const format = new GeoJSON({ featureProjection: 'EPSG:3857' });
+const download = document.getElementById('download');
+source.on('change', function () {
+    const features = source.getFeatures();
+    const json = format.writeFeatures(features);
+    download.href = 'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+});
+
+
 map.addLayer(layer);
 
 // сохраняет положение карты при перезагрузке окна
